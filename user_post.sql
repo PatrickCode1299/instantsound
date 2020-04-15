cfa06a48d058319efa679e0dc9d652f7369d4eda
PatrickCode1299 <63754193+PatrickCode1299@users.noreply.github.com>
2020-04-15 23:25:50 +0100

    Add files via upload

--- user_post.sql
+++ user_post.sql
@@ -1,16 +1,10 @@
 CREATE TABLE user_post(
-userpostid int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
 username text NOT NULL,
 caption text NOT NULL,
-post_day datetime NOT NULL,
-colorbg text NOT NULL,
 image text NOT NULL,
-image1 text NOT NULL,
-audio text NOT NULL,
-status text NOT NULL,
-sharer text NOT NULL,
-unique_id text NOT NULL
-
+post_day timestamp NOT NULL,
+colorbg text NOT NULL
 
 
 
